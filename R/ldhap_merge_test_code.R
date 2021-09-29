0d3afa2683babb703aded7898757a164f59c9d1c
Tim Myers <48717893+timyers@users.noreply.github.com>
2021-09-29 16:25:43 -0400

    Create ldhap_merge_test_code.R

---
+++ R/ldhap_merge_test_code.R
@@ -0,0 +1,17 @@
+df_haplotype <- read.delim(file = "ldhap_haplotype.txt")
+df_variant <- read.delim(file = "ldhap_variant.txt")
+
+
+# transpose dataframe
+df_hap_transpose <- as.data.frame(t(as.matrix(df_haplotype)))
+
+# df_all <- cbind(df_variant, df_hap_transpose)
+
+# combine haplotypes from df_hap_transpose
+df_all <- cbind(df_variant, df_hap_transpose[c(1:3),c(1:4)])
+
+# remove rownames
+rownames(df_all) <- c()
+
+# change column names
+names(df_all)[4:ncol(df_all)] <- "Haplotype"
