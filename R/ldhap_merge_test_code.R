bd74641098bb40735146b0e7b65054d40a270e9c
Tim Myers <48717893+timyers@users.noreply.github.com>
2021-09-30 09:04:42 -0400

    Major update

--- R/ldhap_merge_test_code.R
+++ R/ldhap_merge_test_code.R
@@ -1,6 +1,9 @@
+setwd("/Users/myersta/R/Rscripts/")
+
 df_haplotype <- read.delim(file = "ldhap_haplotype.txt")
 df_variant <- read.delim(file = "ldhap_variant.txt")
 
+num_of_snps <- 3
 
 # transpose dataframe
 df_hap_transpose <- as.data.frame(t(as.matrix(df_haplotype)))
@@ -8,11 +11,28 @@ df_hap_transpose <- as.data.frame(t(as.matrix(df_haplotype)))
 # df_all <- cbind(df_variant, df_hap_transpose)
 
 # combine haplotypes from df_hap_transpose
-df_all <- cbind(df_variant, df_hap_transpose[c(1:3),c(1:4)])
+df_all <- cbind(df_variant, df_hap_transpose[c(1:num_of_snps),c(1:4)])
 
 # remove rownames
 rownames(df_all) <- c()
 
 # change column names
-names(df_all)[4:ncol(df_all)] <- "Haplotype"
-names(df_all)[5:ncol(df_all)] <- "      "
+names(df_all)[4] <- "Haplotypes"
+# names(df_all)[5:ncol(df_all)] <- " "
+
+# create new data frame
+df3 <- df_hap_transpose[(num_of_snps+1):nrow(df_hap_transpose),]
+# Convert row names into first column
+df3 <- cbind(rownames(df3), data.frame(df3, row.names=NULL))
+
+
+# create empty data frame
+df4 <- data.frame(matrix("   ", nrow = 2, ncol = 2))
+# combine df3 & df4
+df5 <- cbind(df4, df3)
+
+# row bind new data frame, df5, to df_all
+df_total <- data.frame(mapply(c, df_all, df5))
+
+# change column names
+names(df_total)[5:ncol(df_total)] <- " "
