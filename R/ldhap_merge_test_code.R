e9ca25ba96d6ddd2f9e5d50bc1069297b3219760
Tim Myers <48717893+timyers@users.noreply.github.com>
2021-09-30 12:19:12 -0400

    Major update

--- R/ldhap_merge_test_code.R
+++ R/ldhap_merge_test_code.R
@@ -1,9 +1,19 @@
-setwd("/Users/myersta/R/Rscripts/")
-
-df_haplotype <- read.delim(file = "ldhap_haplotype.txt")
-df_variant <- read.delim(file = "ldhap_variant.txt")
-
+## data
+# test with 3 snps
+df_haplotype <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_haplotype3.txt")
+df_variant <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_variant3.txt")
 num_of_snps <- 3
+
+# test with 8 snps
+df_haplotype <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_haplotype8.txt")
+df_variant <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_variant8.txt")
+num_of_snps <- 8
+
+# test with 1 snp
+df_haplotype <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_haplotype1.txt")
+df_variant <- read.delim(file = "https://raw.githubusercontent.com/timyers/myworkRscripts/main/R/data/ldhap_variant1.txt")
+num_of_snps <- 1
+
 
 # transpose dataframe
 df_hap_transpose <- as.data.frame(t(as.matrix(df_haplotype)))
@@ -11,17 +21,19 @@ df_hap_transpose <- as.data.frame(t(as.matrix(df_haplotype)))
 # df_all <- cbind(df_variant, df_hap_transpose)
 
 # combine haplotypes from df_hap_transpose
-df_all <- cbind(df_variant, df_hap_transpose[c(1:num_of_snps),c(1:4)])
+df_all <- cbind(df_variant, df_hap_transpose[c(1:num_of_snps),c(1:ncol(df_hap_transpose))])
 
-# remove rownames
+# remove extra rownames
 rownames(df_all) <- c()
 
 # change column names
 names(df_all)[4] <- "Haplotypes"
 # names(df_all)[5:ncol(df_all)] <- " "
 
-# create new data frame
+# create new data frame with Count & Frequency
 df3 <- df_hap_transpose[(num_of_snps+1):nrow(df_hap_transpose),]
+# change row names to "Haplotype_Count" & "Haplotype Frequency"
+###### Insert code here ######
 # Convert row names into first column
 df3 <- cbind(rownames(df3), data.frame(df3, row.names=NULL))
 
